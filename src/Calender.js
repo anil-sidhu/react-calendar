7a084022301af4c7e66dbfe689f23552e2617c37
anil <webanilsidhu@gmail.com>
2019-05-19 17:44:22 +0530

    calender

--- src/Calender.js
+++ src/Calender.js
@@ -8,26 +8,31 @@ class Calender extends React.Component {
   constructor() {
     super()
     this.state = {
-      month: ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'aug', 'spet', 'oct', 'nov', 'dec'],
-      selectedMonth: undefined,
-      selectedMonthNumber: undefined,
+      month: ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec'],
+      selectedMonth: 4,
+      selectedMonthNumber: 2019,
       CalenderDays: undefined,
       currentYear: '2019',
-      selectYear:'2019',
+      selectYear: '2019',
       totalDayMonth: 31,
+      selectedDay:undefined
     }
   }
   days(totalDayMonth) {
     const days = [];
     for (let i = 0; i <= totalDayMonth; i++) {
       days.push(
-        <Col onClick={() => { this.selectDay(i) }} className="day" sm={1}>{i}</Col>
+        <Col key={i}
+          onClick={() => { this.selectDay(i) }}
+          className={this.state.selectedDay==i?"activeDays day" :'day'} 
+          sm={1}>{i}</Col>
       );
     }
     return days;
   }
   selectDay(item) {
-    alert(item)
+    console.warn("result",this.state.selectedMonth,this.state.selectYear,item)
+    this.setState({selectedDay:item})
   }
   componentDidMount() {
     this.setState(
@@ -36,43 +41,41 @@ class Calender extends React.Component {
         selectedMonthNumber: date.getMonth()
       })
   }
-  changeMonth(flag) {
+  async changeMonth(flag) {
+
     if (flag == 'plus' && this.state.selectedMonthNumber <= 12) {
-      this.setState({
+      await this.setState({
         selectedMonthNumber: this.state.selectedMonthNumber + 1,
-        selectedMonth: this.state.month[this.state.selectedMonthNumber]
       })
     }
     else {
       if (this.state.selectedMonthNumber > 0) {
-        this.setState({
+        await this.setState({
           selectedMonthNumber: this.state.selectedMonthNumber - 1,
-          selectedMonth: this.state.month[this.state.selectedMonthNumber]
         })
-        console.warn("else", this.state.selectedMonthNumber)
       }
     }
-    this.daysInMonth(this.state.selectedMonthNumber)
     this.setState({
       selectedMonth: this.state.month[this.state.selectedMonthNumber]
     })
-
+    await this.daysInMonth(this.state.selectedMonthNumber)
   }
-  daysInMonth() {
-    let data = new Date(this.state.selectYear, this.state.selectedMonthNumber, 0).getDate();
-    this.setState({ totalDayMonth: data })
-    console.warn(data, this.state.selectedMonthNumber)
+  async daysInMonth(monthNumber) {
+    let data = new Date(this.state.selectYear, monthNumber, 0).getDate();
+    await this.setState({ totalDayMonth: data })
   }
   years() {
     const yrs = [];
     for (let i = this.state.currentYear; i <= 2022; i++) {
       yrs.push(
-        <Dropdown.Item href="#/action-2" 
-        onClick={() => { this.selectYear(i) }} className="day" sm={1}>
-        {i}
+        <Dropdown.Item href="#/action-2"
+          key={i}
+          onClick={() => { this.selectYear(i) }} className="day" sm={1}>
+          {i}
         </Dropdown.Item>
       );
-    }    
+    }
+
     return (
       <Dropdown>
         <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -85,16 +88,14 @@ class Calender extends React.Component {
     )
   }
 
-  selectYear(yr)
-  {
-    this.setState({selectYear:yr})
+  selectYear(yr) {
+    this.setState({ selectYear: yr })
   }
   render() {
     return (
       <div className="App">
         <Container className="size">
           <Row>
-
             {this.years()}
           </Row>
           <Row>
